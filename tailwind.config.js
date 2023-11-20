0719f949bea9716526da0c2a8b77a8fc3f8e8f63
threeanra <trian.radis448@gmail.com>
2023-11-20 09:46:32 +0700

    Final

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "src/**/*.{html,js}",
+    "node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        "nunito" : ['Nunito'],
+        "opensans" : ['Open Sans']
+      },
+      colors: {
+        "theme" : '#F26440',
+        "theme-green" : '#286F6C'
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+};
