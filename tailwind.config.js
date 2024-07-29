960f007dfafc353fda606067bbc06739b890e04c
TEARSDR0P <trian.radis448@gmail.com>
2024-07-30 00:20:25 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "src/**/*.{html,js}",
-    "node_modules/flowbite/**/*.js"
-  ],
+  content: ["./src/**/*.{html,js}", "node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
-        "nunito" : ['Nunito'],
-        "opensans" : ['Open Sans']
+        nunito: ["Nunito"],
+        opensans: ["Open Sans"],
       },
       colors: {
-        "theme" : '#F26440',
-        "theme-green" : '#286F6C'
-      }
+        theme: "#F26440",
+        "theme-green": "#286F6C",
+      },
     },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ],
+  plugins: [require("flowbite/plugin")],
 };
